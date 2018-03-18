7612e21dd04c74e2e70ec782c66156797669a3cc
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2018-03-18 14:18:24 -0400

    auth

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
