2678c566529aa21e64e8fd5a0713117aaee6e922
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2018-03-19 19:13:46 -0400

    headers

--- server/router/user_routes.js
+++ server/router/user_routes.js
@@ -129,8 +129,8 @@ var log_in = (req, res) => {
 
        
         return User.generateAuthToken().then((token) => {
-            console.log('auth : ', token)
-            res.header('auth', token).send(User);
+            console.log('Auth : ', token)
+            res.header('Auth', token).send(User);
             
         });
 
