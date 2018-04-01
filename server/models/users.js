882caac15900cf44e315b6eb62ac4e2c84507ed9
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2018-04-01 17:39:13 -0400

    display-users with auth

--- server/models/users.js
+++ server/models/users.js
@@ -67,7 +67,7 @@ var UserSchema = new mongoose.Schema({
 UserSchema.methods.toJSON = function() {
     var user = this;
     var userObject = user.toObject();
-    return _.pick(userObject, ['id','email', 'firstName', 'lastName','role']);
+    return _.pick(userObject, ['_id','email', 'firstName', 'lastName','role']);
 };
 
 
