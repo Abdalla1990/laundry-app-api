21ffc0fde368dc45a8836bb7cb1c868b574f3d94
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2018-07-24 23:58:37 -0400

    test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/laundry-app');
 mongoose.connection.on('open', function() {
     mongoose.connection.db.admin().serverStatus(function(error, info) {
-      console.log(info.version);
+      console.log(info);
     });
   });
 module.exports = { mongoose };
